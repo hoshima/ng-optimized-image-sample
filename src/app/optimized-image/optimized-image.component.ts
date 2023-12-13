c82b2a3249f316326e1e89dc3c732931935cdb66
hoshima <hoshima@users.noreply.github.com>
2023-12-13 23:17:22 +0900

    画像参照先をimgixに変更

--- src/app/optimized-image/optimized-image.component.ts
+++ src/app/optimized-image/optimized-image.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, provideImgixLoader } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 
@@ -6,14 +6,8 @@ import { NgOptimizedImage } from '@angular/common';
   selector: 'app-optimized-image',
   standalone: true,
   imports: [CommonModule, NgOptimizedImage],
-  template: `
-    <img
-      ngSrc="./assets/images/ei_compressed.jpg"
-      width="1024"
-      height="768"
-      priority
-    />
-  `,
+  providers: [provideImgixLoader('https://hoshima.imgix.net/')],
+  template: ` <img ngSrc="ei.jpg" width="1024" height="768" priority /> `,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
